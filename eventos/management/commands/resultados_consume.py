955bf38cfb5372bf32928e0ec2680ad2ae0e70f5
Gabs-489 <141871264+Gabs-489@users.noreply.github.com>
2025-03-12 19:19:42 -0500

    Delete eventos directory

--- eventos/management/commands/resultados_consume.py
+++
@@ -1,8 +0,0 @@
-from django.core.management.base import BaseCommand
-from eventos.services.archivo_consumer import iniciar_consumer  # Importa la función corregida
-
-class Command(BaseCommand):
-    help = "Inicia el consumidor de RabbitMQ"
-
-    def handle(self, *args, **kwargs):
-        iniciar_consumer()
--- eventos/urls.py
+++
@@ -1,8 +0,0 @@
-from .views import *
-from django.urls import path
-
-urlpatterns = [
-    path('eventos/EEG', pag_inicial),
-    path('eventos/EEG/analisis', analisis_eeg,name='analisis_eeg'),
-    path('eventos/EEG/resultados', resultados_eeg, name='resultados_eeg')
-]
