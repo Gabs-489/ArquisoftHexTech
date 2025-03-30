387e3c0d9c283b891767159bd1b4e2500a5de490
Gabriela Zambrano <g.zambranoz@uniandes.edu.co>
2025-03-30 14:56:57 -0500

    G

--- ServidorEventos/eventos/serializers.py
+++ ServidorEventos/eventos/serializers.py
@@ -5,5 +5,5 @@ from . import models
 class Examen_serializer(serializers.ModelSerializer):
 
     class Meta:
-        fields = ('id', 'fecha', 'nombre', 'celular', 'peso_archivo', 'path','resultado_analisis')
+        fields = ('id', 'fecha', 'nombre', 'peso_archivo', 'path','resultado_analisis')
         model = models.EEG
