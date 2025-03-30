907507eca3b0498c6b0b6cf2b69c354e5fbe579d
Gabriela Zambrano <g.zambranoz@uniandes.edu.co>
2025-03-30 17:36:49 -0500

    G

--- InterfazUsuario/usuarios/views.py
+++ InterfazUsuario/usuarios/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 import requests
 
@@ -11,8 +12,10 @@ def pag_principal(request):
 def todas_historias_clinicas(request):
     response = requests.get(f"{MICROSERVICIO_USUARIOS_URL}/historias_usuario", timeout=20)
     print(response)
+     return HttpResponse("Some response text")
 
 def historia_clinica_por_paciente(request):
     numero_identidad = 0
     response = requests.get(f"{MICROSERVICIO_USUARIOS_URL}/historias_usuario/{numero_identidad}", timeout=20)
     print(response)
+     return HttpResponse("Some response text")
