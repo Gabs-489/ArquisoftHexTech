2922cf9a2dcbddbaa5b2074c748e2bef02d86fd4
Gabriela Zambrano <g.zambranoz@uniandes.edu.co>
2025-03-30 17:30:00 -0500

    G

--- InterfazUsuario/usuarios/urls.py
+++ InterfazUsuario/usuarios/urls.py
@@ -5,5 +5,5 @@ from .views import *
 urlpatterns = [
     path('interfaz/usuarios', pag_principal),
     path('interfaz/usuarios/historiasClinicas', todas_historias_clinicas),
-    path('interfaz/usuarios/HC_paciente', historia_clinica_por_paciente = 'HC_paciente'),
+    path('interfaz/usuarios/HC_paciente', historia_clinica_por_paciente , name = 'HC_paciente'),
 ]
