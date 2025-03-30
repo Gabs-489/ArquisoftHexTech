e1d78ab198310c705ad20a39e59db15172e79208
Gabriela Zambrano <g.zambranoz@uniandes.edu.co>
2025-03-30 14:24:36 -0500

    G

--- ServidorUsuarios/usuarios/urls.py
+++ ServidorUsuarios/usuarios/urls.py
@@ -2,6 +2,6 @@ from .views import *
 from django.urls import path
 
 urlpatterns = [
-    path('usuarios/pacientes/',mostrar_pacientes),
+    path('usuarios/pacientes',mostrar_pacientes),
     path('usuarios/pacientes/<str:numero_identidad_paciente>', obtener_examenes_paciente, name='obtener_examenes_paciente'),
 ]
