a89929b52ddcc7179c4f0fb951d2e94d2a9706c1
Gabriela Zambrano <g.zambranoz@uniandes.edu.co>
2025-03-31 10:16:23 -0500

    G

--- InterfazUsuario/usuarios/views.py
+++ InterfazUsuario/usuarios/views.py
@@ -7,15 +7,34 @@ from InterfazUsuario.settings import MICROSERVICIO_USUARIOS_URL
 # Create your views here.
 
 def pag_principal(request):
-    return render(request, 'usuarios/borrador.html')
+    return render(request, 'usuarios/pag_principal.html')
 
 def todas_historias_clinicas(request):
     response = requests.get(f"{MICROSERVICIO_USUARIOS_URL}/historias_usuario", timeout=20)
-    print(response)
-    return HttpResponse("Some response text")
+    historias = response.json()
+    context = {
+        'historiasClinicas' : historias
+        }
+    return render(request, 'usuarios/HistoriasClinicas.html',context)
 
 def historia_clinica_por_paciente(request):
-    numero_identidad = 0
-    response = requests.get(f"{MICROSERVICIO_USUARIOS_URL}/historias_usuario/{numero_identidad}", timeout=20)
-    print(response)
-    return HttpResponse("Some response text")
+
+    if request.method == 'POST':
+                numero_identidad_paciente = request.POST.get('numero_identidad')
+                if numero_identidad_paciente:
+                    response = requests.get(f"{MICROSERVICIO_USUARIOS_URL}/historias_usuario/{numero_identidad_paciente}", timeout=20)
+                    if response.status_code != 200:
+                        mensaje = "No se encontro un paciente con ese numero de identidad."
+                        return HttpResponse(f"""<script>
+                                        alert("{mensaje}");
+                                        window.location.href = "/interfaz/eventos";  // Redirigir a la página principal o donde desees
+                                        </script>
+                                        """) 
+                    else: 
+                        historias = response.json()
+                        context = {
+                                    'historiasClinicas' : historias
+                                    }
+                        return render(request, 'usuarios/HistoriaClinicasPaciente.html',context)
+    
+    return render(request, 'usuarios/HistoriaClinicasPaciente.html')
