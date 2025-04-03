4797f5e1069823d3b4df2e834e8843bfe1e5670f
mjmantillav24 <140014572+mjmantillav24@users.noreply.github.com>
2025-04-03 14:04:29 -0500

    Update settings.py

--- ServidorUsuarios/ServidorUsuarios/settings.py
+++ ServidorUsuarios/ServidorUsuarios/settings.py
@@ -134,4 +134,4 @@ MEDIA_ROOT = os.path.join(PROJECT_ROOT, 'static', 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-HISTORIAS_CLINICAS_API = "http://" + os.environ.get("HC_API", "34.41.183.182") + ":" + os.environ.get("HC_PORT", "8080")
+HISTORIAS_CLINICAS_API = "http://" + os.environ.get("HC_API", "34.44.198.82") + ":" + os.environ.get("HC_PORT", "8080")
