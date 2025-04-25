f925760c261f397b0c515fa65be1fca29855b1e5
mjmantillav24 <140014572+mjmantillav24@users.noreply.github.com>
2025-04-25 13:11:51 -0500

    Update views.py

--- InterfazUsuario/usuarios/views.py
+++ InterfazUsuario/usuarios/views.py
@@ -32,7 +32,7 @@ def todas_historias_clinicas(request):
 
     except Exception as e:
         mensaje = f"Error al obtener historias clínicas: {str(e)}"
-
+        print(mensaje)
         if mensaje == "Service Temporarily Unavailable for url: http://10.128.0.8:8000/usuarios/historias_usuario":
             print("funcion mandar mensaje")
             mandar_mensaje_advertencia()
