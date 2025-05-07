0d39a3ddd42d37a562ac469e0c968b2abd3d858d
aarevalom0 <a.arevalom@uniandes.edu.co>
2025-05-07 10:15:53 -0500

    ASR 18

--- ServidorUsuarios/usuarios/urls.py
+++ ServidorUsuarios/usuarios/urls.py
@@ -4,6 +4,7 @@ from django.urls import path
 urlpatterns = [
     path('usuarios/pacientes',mostrar_pacientes),
     path('usuarios/pacientes/<str:numero_identidad_paciente>', obtener_examenes_paciente, name='obtener_examenes_paciente'),
+    path('usuarios/pacientes/nuevo/<str:numero_identidad_paciente>', agregar_evento_a_paciente, name='obtener_examenes_paciente'),
     path('usuarios/historias_usuario', obtener_historias_de_pacientes, name='obtener_historias_de_pacientes'),
     path('usuarios/historias_usuario/<str:numero_identidad_paciente>', obtener_historia_de_un_paciente, name='obtener_historia_de_un_paciente'),
 ]
