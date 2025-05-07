87ea4e3f975f2db17ea6f0113519f1fe0505019f
Gabriela Zambrano <g.zambranoz@uniandes.edu.co>
2025-05-07 11:39:35 -0500

    G

--- InterfazUsuario/eventos/urls.py
+++ InterfazUsuario/eventos/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('interfaz/eventos/consultar', get_evento),
     path('interfaz/eventos/EEG',pag_paciente_examenes),
     path('interfaz/eventos/EEG/analisis', analisis_eeg,name='analisis_eeg'),
-    path('interfaz/eventos/EEG/resultados', resultados_eeg, name='resultados_eeg')
+    path('interfaz/eventos/EEG/resultados', resultados_eeg, name='resultados_eeg'),
+    path('interfaz/eventos/nuevo', nuevo_evento, name='nuevo_evento')
 ]
