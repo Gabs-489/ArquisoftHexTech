afc6b140aaee845f1c736cd6858847931658e09a
Gabriela Zambrano <g.zambranoz@uniandes.edu.co>
2025-05-28 16:46:24 -0500

    G

--- ServidorUsuarios/ServidorUsuarios/views.py
+++ ServidorUsuarios/ServidorUsuarios/views.py
@@ -1,15 +1,17 @@
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from django.db import connection
+from pymongo import MongoClient
+
+from ServidorUsuarios.settings import MONGO_CLI
 
 def index(request):
     return HttpResponse(status=204)
 
 def health_check(request):
     try:
-        # Intentamos hacer un query simple
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT 1")
+        client = MongoClient(MONGO_CLI, serverSelectionTimeoutMS=1000)
+        client.admin.command('ping')  
 
         return JsonResponse({'status': 'ok'}, status=200)
     except Exception as e:
